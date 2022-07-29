cf128f9da6690a3accbe7658400179103e2f8e09
xNewz <pargornruasijan@gmail.com>
2022-07-30 02:55:04 +0700

    update: change to orm

---
+++ model.py
@@ -0,0 +1,21 @@
+from peewee import *
+from sqlalchemy import ForeignKey
+
+db = SqliteDatabase('vote_orm.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Topics(BaseModel):
+    id = CharField(max_length=60, null=False, primary_key=True)
+    name = TextField()
+
+
+class Votes(BaseModel):
+    id = AutoField(primary_key=True, null=False)
+    topic = ForeignKeyField(Topics, backref='topic')
+    choice_name = TextField()
+    choice_count = IntegerField(default=0)
